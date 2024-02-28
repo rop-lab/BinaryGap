b4d7d9467d66f5c2045762a55ef8b642672851a3
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-02-28 04:27:11 +0300

    solution

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-function solution(N){
+// declare the function
+function solution(N) {
     const binaryStr = N.toString(2);
-
-    let maxGap = 0;
+  
+    // iterate through the variables
+    let longestGap = 0;
     let currentGap = 0;
-
-    // Iterate the  string from left to right, counting gaps and updating max gap if necessary
-    for (const char of binaryStr){
-        if (char === '1'){
-            
-            maxGap = Math.max(maxGap, currentGap);
-            currentGap = 0;
-        } else {
-            currentGap++;
+    let counting = false;
+  
+    for (let x of binaryStr) {
+      if (x === '0') {
+        if (counting) {
+          currentGap += 1;
         }
+      } else {
+        if (counting) {
+          longestGap = Math.max(longestGap, currentGap); // to give the longestGap
+          currentGap = 0;
+        } else {
+          counting = true;
+        }
+      }
     }
-    return maxGap + 1;   // Add one because we are looking at
-    // the number of bits between ones not
-}
-const N = (529);
-console.log(solution(N)); 
-// Output : 4
-            
+  
+    return longestGap;
+  }
+  
+  // Call the function
+  let result = solution(529);
+  console.log(result); // Output: 4
+  
